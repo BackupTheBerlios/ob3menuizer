78c66e0d0cdbb23f45842af819412d94dec40898
gbhilden <gbhilden>
2005-06-15 20:20:51 +0000

    updated installer.

--- 1.0/installer.sh
+++ 1.0/installer.sh
@@ -21,6 +21,8 @@ fi
 #Default install path.  Edit below to your liking if desired
 BINPATH="${BINPATH:=/usr/local/bin}"
 MYMANPATH="${MYMANPATH:=/usr/local/man/man1}"
+LILOPATH="${LILOPATH:=/etc/lilo.conf}"
+PORTAGEPATH="${PORTAGEPATH:=/etc/portage}"
 
 # Check that directories exist.  
 if [ -d "${BINPATH}" ]; then
@@ -33,14 +35,35 @@ fi
 if [ -d "${MYMANPATH}" ]; then
 	./resource/echoc "Man directory path is good." green
 else
-	./resource/echoc "Can't find $MYMANPATH .  Install will exit." red
+	./resource/echoc "Can't find $MYMANPATH .  Install will exit. Please edit the install script with the correct man page path." red
 	exit
 fi
 
+# if all is good, copy the files
 cp ./resource/ob3_editor $BINPATH
 cp ./resource/ob3_term $BINPATH
+cp ./resource/ob3_term_edit $BINPATH
 cp ./resource/openbox-autogen_menu $BINPATH
-#cp ./resource/ob3.png $HOME/.config/openbox/
+cp ./resource/edit_fstab $BINPATH
+
+#using lilo?
+if [ -e "${LILOPATH}" ]; then
+	cp ./resource/edit_lilo $BINPATH
+fi
+
+#using Gentoo?
+if [ -d "${PORTAGEPATH}" ]; then
+	cp ./resource/edit_make $BINPATH
+	cp ./resource/edit_package.keywords $BINPATH
+	cp ./resource/edit_package.use $BINPATH
+fi
+
+#ob3 icon 
+if [ -d "$HOME/.config/openbox" ]; then
+	cp ./resource/ob3.png $HOME/.config/openbox/
+else
+	cp ./resource/ob3.png $HOME
+fi
 
 
 ./resource/echoc "Program files successfully installed.  Installing documentation and man page." green
